273ade7c076940c0cf162586f456ac37e1dc063c
aliceclimas <alicecristinalima31@gmail.com>
2025-05-30 11:39:32 -0300

    aula 11 finalizada

--- estrutura-de-dados-com-java/src/DefaultPackage/vetor/teste/Aula11.java
+++ estrutura-de-dados-com-java/src/DefaultPackage/vetor/teste/Aula11.java
@@ -1,16 +1,14 @@
 package src.DefaultPackage.vetor.teste;
 
+import src.DefaultPackage.vetor.Lista;
 import src.DefaultPackage.vetor.VetorObjetos;
 
 public class Aula11 {
 
     public static void main(String[] args){
 
-        VetorObjetos vetor = new VetorObjetos(2);
+        Lista<String> vetor = new Lista<String>(1);
+        vetor.adiciona("Elemento");
 
-        vetor.adiciona(1);
-        vetor.adiciona("Elemento do tipo string");
-
-        System.out.println(vetor);
     }
 }
