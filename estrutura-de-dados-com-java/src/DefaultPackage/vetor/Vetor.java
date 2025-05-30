bb0a65b29d68c0b9508165d7ca647d93bc45361a
aliceclimas <alicecristinalima31@gmail.com>
2025-05-30 10:29:36 -0300

    aula 08 finalizada

--- estrutura-de-dados-com-java/src/DefaultPackage/vetor/Vetor.java
+++ estrutura-de-dados-com-java/src/DefaultPackage/vetor/Vetor.java
@@ -31,7 +31,32 @@ public class Vetor {
         return false;
     }
 
-//    public String busca(int posicao){
+    public boolean adiciona(int posicao, String elemento){
+        this.aumentaCapacidade();
+        if (!(posicao >= 0 && posicao < tamanho)){
+            throw new IllegalArgumentException("Posição inválida");
+        }
+        this.aumentaCapacidade();
+        // mover todos os elementos
+        for (int i =this.tamanho-1;i>=posicao;i--){
+            this.elementos[i+1] = this.elementos[i];
+        }
+        this.elementos[posicao] = elemento;
+        this.tamanho++;
+
+        return true;
+    }
+    private void aumentaCapacidade(){
+        if (this.tamanho == this.elementos.length){
+            String[] elementosNovos = new String[this.elementos.length * 2];
+            for (int i = 0;i<this.elementos.length;i++){
+                elementosNovos[i] = this.elementos[i];
+            }
+            this.elementos = elementosNovos;
+        }
+    }
+
+//    public String busca(int posicao){ //aula05
 //        if (!(posicao >=0 && posicao < tamanho)){
 //            throw new IllegalArgumentException("Posição inválida");
 //        }
