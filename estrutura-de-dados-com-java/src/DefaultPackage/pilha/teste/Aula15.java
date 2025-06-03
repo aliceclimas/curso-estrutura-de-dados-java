332d637de1169af1ed5ebbc4979ce86708534a0f
aliceclimas <alicecristinalima31@gmail.com>
2025-06-03 08:18:52 -0300

    aula 15 finalizada

---
+++ estrutura-de-dados-com-java/src/DefaultPackage/pilha/teste/Aula15.java
@@ -0,0 +1,17 @@
+package src.DefaultPackage.pilha.teste;
+
+import src.DefaultPackage.pilha.Pilha;
+
+public class Aula15 {
+
+    public static void main(String[] args){
+
+        Pilha<Integer> pilha = new Pilha<Integer>();
+
+        System.out.println(pilha.estaVazia());
+
+        pilha.empilha(1);
+
+        System.out.println(pilha.estaVazia());
+    }
+}
