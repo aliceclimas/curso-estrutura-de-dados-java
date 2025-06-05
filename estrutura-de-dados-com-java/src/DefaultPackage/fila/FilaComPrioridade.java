fcda8eee002835c5c1b485ba20d71279fa2376fb
aliceclimas <alicecristinalima31@gmail.com>
2025-06-05 11:34:39 -0300

    aula 24 finalizada

--- estrutura-de-dados-com-java/src/DefaultPackage/fila/FilaComPrioridade.java
+++ estrutura-de-dados-com-java/src/DefaultPackage/fila/FilaComPrioridade.java
@@ -1,4 +1,20 @@
 package src.DefaultPackage.fila;
 
-public class FilaComPrioridade {
+public class FilaComPrioridade<T> extends Fila<T>{
+
+    public void enfileira(T elemento){
+
+      Comparable<T> chave = (Comparable<T>) elemento;
+
+      int i;
+
+      for (i = 0;i<this.tamanho;i++){
+          if (chave.compareTo(this.elementos[i]) < 0){
+              break;
+          }
+      }
+      this.adiciona(i, elemento);
+
+    }
+
 }
