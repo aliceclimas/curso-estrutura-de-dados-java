dbb74aa3f95a3775e86518126cb65f46a145947a
aliceclimas <alicecristinalima31@gmail.com>
2025-06-06 10:57:58 -0300

    aula 26 finalizada

---
+++ estrutura-de-dados-com-java/src/DefaultPackage/lista/teste/ListaEncadeadaTeste.java
@@ -0,0 +1,17 @@
+package src.DefaultPackage.lista.teste;
+
+import src.DefaultPackage.lista.ListaEncadeada;
+
+public class ListaEncadeadaTeste {
+
+    public static void main(String[] args){
+
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+
+        lista.adiciona(1);
+
+        System.out.println(lista);
+
+
+    }
+}
